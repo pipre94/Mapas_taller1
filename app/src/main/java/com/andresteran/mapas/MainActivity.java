043eebc6ec418f6b5b82bef1a541e894b33d7091
AndresTeran <pipre94@gmail.com>
2017-05-16 13:37:37 -0500

    asf

--- app/src/main/java/com/andresteran/mapas/MainActivity.java
+++ app/src/main/java/com/andresteran/mapas/MainActivity.java
@@ -3,11 +3,34 @@ package com.andresteran.mapas;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback{
+
+    GoogleMap m_maps;
+    boolean mapReady = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        MapFragment mapFragment =(MapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mapReady =true;
+        LatLng pasto = new LatLng(1.2089284, -77.2779443);
+        CameraPosition target = CameraPosition.builder().target(pasto).zoom(15).build();
+        m_maps.moveCamera(CameraUpdateFactory.newCameraPosition(target));
     }
 }
